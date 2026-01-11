5a836f2cd2c7216f9b52ce4853acdd2808d196fb
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:54:34 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "network_manager_verifier_workspaces" {
+  description = "All network_manager_verifier_workspace resources"
+  value       = azurerm_network_manager_verifier_workspace.network_manager_verifier_workspaces
+}
+
