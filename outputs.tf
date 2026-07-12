86488cc6192debd5e4911d8a095d8d9ae9273348
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "network_manager_verifier_workspaces_id" {
+  description = "Map of id values across all network_manager_verifier_workspaces, keyed the same as var.network_manager_verifier_workspaces"
+  value       = { for k, v in azurerm_network_manager_verifier_workspace.network_manager_verifier_workspaces : k => v.id }
+}
 output "network_manager_verifier_workspaces_description" {
   description = "Map of description values across all network_manager_verifier_workspaces, keyed the same as var.network_manager_verifier_workspaces"
   value       = { for k, v in azurerm_network_manager_verifier_workspace.network_manager_verifier_workspaces : k => v.description }
